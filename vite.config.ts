27c4d5fa871954b25ecc4abfb768ebe97538abdc
lruij <1012232443@qq.com>
2023-05-26 17:53:17 +0800

    feat: 打包文件大小解析 visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { visualizer } from 'rollup-plugin-visualizer'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    // https://www.npmjs.com/package/rollup-plugin-visualizer
+    visualizer({ open: true }),
   ],
   build: {
     chunkSizeWarningLimit: 2000,
