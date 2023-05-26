d83277064a01f906b5874cf44078c5e905a2e50f
lruij <1012232443@qq.com>
2023-05-26 22:14:22 +0800

    feat: pinia 基本属性和方法使用

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,85 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+   <div>Pinia state: {{  Test.current }} -- {{ Test.name }}</div>
+   <div>响应式解构: {{  current }} -- {{ name }}</div>
+   <button @click="change">$patch 方法：改变数据</button>
+  <br/>
+   <div>Pinia actions: {{  Test.user.name }} -- {{  Test.user.age }}</div>
+   <button @click="staticChange">同步改变数据</button>
+   <br>
+   <div>Pinia actions:
+    <ul>
+      <li v-for="item in Test.data" :key="item.id">{{ item.desc }}</li>
+    </ul>
+   </div>
+   <button @click="asyncChange">异步改变数据</button>
+
+   <br>
+   <div>Pinia getters: {{ Test.newName }}</div>
+
+   <br>
+   <button @click="Test.$reset()">重置数据</button>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup lang='ts'>
+import { useTestStore } from './store'
+import { storeToRefs } from 'pinia'
+
+const Test = useTestStore()
+
+const change = () => {
+  // #1、直接修改值
+  // Test.current++
+
+  // #2、通过 $patch 传对象
+  Test.$patch({
+    current: 1222,
+    name: '改变了'
+  })
+
+  // #3、通过 $patch 传函数 【推荐】
+  // Test.$patch((state) => {
+  //   state.current++
+  // })
+
+  // #4、通过 $state 覆盖所有值
+  // Test.$state = {
+  //   current: 2222,
+  //   name: '$state'
+  // }
+
+  // 5、通过 action 【推荐】
+  // Test.setCurrent(999)
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+// 数据解构
+const { current, name} = storeToRefs(Test)
+
+
+const staticChange = () => {
+  Test.setUser()
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+const asyncChange = () => {
+  Test.getData()
 }
+
+// 监听数据改变
+Test.$subscribe((args, state) => {
+  console.log('--- $subscribe ----');
+  console.log('==> ', args);
+  console.log('==> ', state);
+})
+
+Test.$onAction((args) => {
+  args.after(() => {
+    console.log('after');
+  })
+  console.log('--- $onAction ----');
+  console.log(args);
+
+})
+</script>
+
+<style lang='scss' scoped>
+
 </style>
---
+++ src/store/index.ts
@@ -0,0 +1,74 @@
+import { defineStore } from 'pinia'
+import { Names } from './store-name'
+
+
+// 同步
+type User = {
+  name: string,
+  age: number
+}
+
+const result: User = {
+  name: '同步',
+  age: 18
+}
+
+// 异步
+type List = {
+  id: number,
+  desc: string
+}
+
+const listData: List[] = [
+  {
+    id: 1,
+    desc: '第一行'
+  }
+]
+
+
+const getListData = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(listData)
+    }, 2000)
+  })
+}
+
+
+export const useTestStore = defineStore(Names.TEST, {
+  state: () => {
+    return {
+      current: 1,
+      name: 'Test',
+      user: <User>{},
+      data: <List[]>[]
+    }
+  },
+  // computed 修饰一些值
+  getters: {
+    newName():string {
+      return `$---${this.name}`
+    }
+  },
+  // methods 支持同步和异步 提交 state
+  actions: {
+    setCurrent (num: number) {
+      this.current = num
+    },
+
+    setUser() {
+      this.user = result
+    },
+
+    async getData() {
+      this.data = await getListData() as List[]
+      this.updateName('数据获取成功')
+    },
+
+    updateName(name: string) {
+      this.name = name
+    }
+  }
+})
+
